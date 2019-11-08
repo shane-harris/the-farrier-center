511dc38677d543ef805b6c2b9533d6cd20e6fe14
Michael Dorst <dorstm@ecs.csus.edu>
2019-11-08 11:03:28 -0800

    Format index.js (prettier)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+  res.send('Hello World!')
+})
 
 app.listen(8000, () => {
-    console.log('Example app listening on port 8000!')
-});
+  console.log('Example app listening on port 8000!')
+})
