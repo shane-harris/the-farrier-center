a71a22f02280aacd00e3b73c6bdf6b0bffb46531
Michael Dorst <dorstm@ecs.csus.edu>
2019-11-26 20:56:45 -0800

    Add additional tests

--- test/routes.js
+++ test/routes.js
@@ -7,17 +7,50 @@ const app = require('../index')
 
 describe('Routes', () => {
   describe('GET /horses', () => {
-    let getHorses
+    let req
     before(() => {
-      getHorses = chai.request(app).get('/horses')
+      return (req = chai.request(app).get('/horses'))
     })
 
     it('should get the horses page', () => {
-      getHorses.then(res => res.should.have.status(200))
+      return req.then(res => res.should.have.status(200))
     })
     describe('The horses page', () => {
       it('should render HTML', () => {
-        getHorses.then(res => res.should.be.html)
+        return req.then(res => res.should.be.html)
+      })
+    })
+  })
+
+  describe('GET /horse', () => {
+    let req
+    before(() => {
+      return (req = chai.request(app).get('/horse'))
+    })
+
+    it('should get the horse page', () => {
+      return req.then(res => res.should.have.status(200))
+    })
+    describe('The horses page', () => {
+      it('should render HTML', () => {
+        return req.then(res => res.should.be.html)
+      })
+    })
+  })
+
+  describe('GET /password', () => {
+    let req
+    before(() => {
+      return (req = chai.request(app).get('/password'))
+    })
+
+    it('should get the password page', () => {
+      return req.then(res => res.should.have.status(200))
+    })
+
+    describe('The password page', () => {
+      it('should render HTML', () => {
+        return req.then(res => res.should.be.html)
       })
     })
   })
