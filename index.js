6cc4466caa2c8769107b4d00f4acd6ef0b3c507d
Edward Hall <halle@ecs.csus.edu>
2019-12-05 13:40:37 -0800

    add mongodb.Atlas connection string

--- index.js
+++ index.js
@@ -2,7 +2,23 @@
 
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+try {
+  mongoose.connect(
+    'mongodb+srv://' +
+      process.env.MONGO_USER +
+      ':' +
+      process.env.MONGO_PASS +
+      '@farrier-dev-test-2pgqu.mongodb.net/test?retryWrites=true&w=majority'
+  )
+} catch (error) {
+  console.log(error)
+}
+
+mongoose.connection.on('error', err => {
+  console.log(err)
+})
 
 app.use('/public', express.static('public'))
 
