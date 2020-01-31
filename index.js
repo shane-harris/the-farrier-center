742150e62e7295a0302f0fece7706d195702daf2
Michael Dorst <dorstm@ecs.csus.edu>
2020-01-31 09:51:48 -0800

    Implement user login

--- index.js
+++ index.js
@@ -2,9 +2,39 @@
 
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const mongoose = require('mongoose')
-const app = express()
 const config = require('./config/config.js')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('cookie-session')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const flash = require('connect-flash')
+
+const app = express()
+
+app.use('/public', express.static('public'))
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(session({ keys: ['secretkey1', 'secretkey2', '...'] }))
+app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+const Account = require('./models/account')
+passport.use(new LocalStrategy(Account.authenticate()))
+
+passport.serializeUser(Account.serializeUser())
+passport.deserializeUser(Account.deserializeUser())
 
 let encoded_connection_url =
   'mongodb+srv://' +
@@ -21,9 +51,9 @@ mongoose
     process.exit(1)
   })
 
-const Horse = require('./models/horse')
+mongoose.set('useCreateIndex', true)
 
-app.use('/public', express.static('public'))
+const Horse = require('./models/horse')
 
 app.get('/', (req, res) => {
   res.redirect('/queue')
@@ -53,6 +83,72 @@ app.get('/queue', (req, res) => {
     .sort({ lastVisit: 1 })
     .then(horses => res.render('queue.ejs', { horses }))
     .catch(console.error)
+})
+
+app.get('/register', function(req, res) {
+  res.render('register.ejs', {})
+})
+
+app.post('/register', function(req, res, next) {
+  console.log('registering user')
+  Account.register(new Account({ username: req.body.username }), req.body.password, function(err) {
+    if (err) {
+      console.log('error while user register!', err)
+      return next(err)
+    }
+
+    console.log('user registered!')
+
+    res.redirect('/login')
+  })
+})
+
+app.get('/login', function(req, res) {
+  res.render('login.ejs', { user: req.user, message: req.flash('error') })
+})
+
+app.post(
+  '/login',
+  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/')
+  }
+)
+
+app.get('/logout', function(req, res) {
+  req.logout()
+  res.redirect('/')
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    res.render('error.ejs', {
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  res.render('error.ejs', {
+    message: err.message,
+    error: {}
+  })
 })
 
 app.listen(config.port, () => {
---
+++ models/account.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  passportLocalMongoose = require('passport-local-mongoose')
+
+const Account = new Schema({
+  nickname: String,
+  birthdate: Date
+})
+
+Account.plugin(passportLocalMongoose)
+
+module.exports = mongoose.model('Account', Account)
