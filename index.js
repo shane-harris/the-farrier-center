5eee9186d4e0e58b75d3828f09be66ae7da8c3a7
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-05 20:42:34 -0800

    Cleanup and refactor
    Remove unnecessary call to 'dotenv' in 'index.js'
    Extract static '/public' route to 'routes.js'
    Add documenting comments to 'index.js'
    Move mongo connection to bottom of 'index.js'

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict'
 
-require('dotenv').config()
 const config = require('./config/config.js')
 const cookieParser = require('cookie-parser')
 const express = require('express')
@@ -14,10 +13,9 @@ const session = require('cookie-session')
 
 const app = express()
 
-app.use('/public', express.static('public'))
-
 app.set('views', path.join(__dirname, 'views'))
 
+// Set up various middleware
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -25,27 +23,19 @@ app.use(cookieParser())
 app.use(session({ keys: ['secretkey1', 'secretkey2', '...'] }))
 app.use(flash())
 
+// Passport middleware is used for user session tracking and authentication
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Establish how passport will go about authenticating users
 const User = require('./models/user')
 passport.use(new LocalStrategy(User.authenticate()))
 
+// Establish how passport will send user information over the network
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-mongoose
-  .connect(config.mongo_url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .then(() => console.log('Connected to Database!'))
-  .catch(err => {
-    console.error(err)
-    process.exit(1)
-  })
-
+// Configure routes, as specified in 'routes.js'
 app.use(require('./routes'))
 
 // catch 404 and forward to error handler
@@ -79,6 +69,20 @@ app.use((err, req, res) => {
   })
 })
 
+// Connect to the database
+mongoose
+  .connect(config.mongo_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('Connected to Database!'))
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
+
+// Listen for user requests
 app.listen(config.port, () => {
   console.log(`Example app listening on port ${config.port}!`)
 })
