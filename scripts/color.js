5c61acefae35268db1e260414312afd65abb6f73
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-12 23:52:49 -0800

    Add forest theme colors (commented out)

--- scripts/color.js
+++ scripts/color.js
@@ -1,5 +1,20 @@
 'use strict'
 
+// Forest theme
+/*
+const color = {
+  nav: '#3A2B29',
+  bg: '#232528',
+  fg: '#3A2B29',
+  accent: '#FFC535',
+  border: '#386150',
+  primaryText: '#386150',
+  secondaryText: '#000',
+  tertiaryText: '#fff'
+}
+*/
+
+// Grayscale theme
 const color = {
   nav: '#222',
   bg: '#333',
