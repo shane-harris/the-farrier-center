9ad6bcd6c6aa232a90dd6deceb7b3327c94c6382
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-13 11:01:28 -0800

    Set default color for users who's theme is not set

--- middleware/style.js
+++ middleware/style.js
@@ -4,7 +4,8 @@ const { colorStyle, themes } = require('../scripts/color')
 
 // Middleware that will import 'style' into every EJS file
 module.exports = (req, res, next) => {
-  res.locals.style = colorStyle(req.user.theme)
-  res.locals.themes = themes(req.user.theme)
+  const theme = req.user !== undefined ? req.user.theme : 'Dark'
+  res.locals.style = colorStyle(theme)
+  res.locals.themes = themes(theme)
   next()
 }
--- scripts/color.js
+++ scripts/color.js
@@ -1,16 +1,6 @@
 'use strict'
 
 const themes = {
-  Forest: {
-    nav: '#3A2B29',
-    bg: '#232528',
-    fg: '#3A2B29',
-    accent: '#FFC535',
-    border: '#386150',
-    primaryText: '#386150',
-    secondaryText: '#000',
-    tertiaryText: '#fff'
-  },
   Dark: {
     nav: '#222',
     bg: '#333',
@@ -20,17 +10,26 @@ const themes = {
     primaryText: '#aaa',
     secondaryText: '#000',
     tertiaryText: '#fff'
+  },
+  Forest: {
+    nav: '#3A2B29',
+    bg: '#232528',
+    fg: '#3A2B29',
+    accent: '#FFC535',
+    border: '#386150',
+    primaryText: '#386150',
+    secondaryText: '#000',
+    tertiaryText: '#fff'
   }
 }
 
 const themeList = userTheme =>
   Object.keys(themes).map(theme => {
-    console.log(theme, userTheme)
     return { name: theme, selected: theme === userTheme ? true : false }
   })
 
 const colorStyle = theme => {
-  const color = themes[theme]
+  const color = theme !== undefined ? themes[theme] : themes.Dark
   return `
   :root {
     --nav-color: ${color.nav};
