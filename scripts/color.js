1b2339fa03ba8f3f94aa06eae3942fdbfbc76011
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-12 22:37:03 -0800

    Recolor horses page

--- scripts/color.js
+++ scripts/color.js
@@ -1,13 +1,21 @@
 'use strict'
 
-const navColor = '#3A2B29'
-const bgColor = '#232528'
+const nav = '#3A2B29'
+const bg = '#232528'
+const fg = '#3A2B29'
+const accent = '#FFC535'
+const border = '#386150'
+const fgText = '#386150'
 
 module.exports = () => {
   return `
   :root {
-    --nav-color: ${navColor};
-    --bg-color: ${bgColor};
+    --nav-color: ${nav};
+    --bg-color: ${bg};
+    --fg-color: ${fg};
+    --accent-color: ${accent};
+    --border-color: ${border};
+    --fg-text-color: ${fgText};
   }
   `
 }
