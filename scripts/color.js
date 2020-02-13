a847f4260827521baf9d6a747b9eb2a8e62a56ed
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-12 23:17:35 -0800

    Style forms

--- scripts/color.js
+++ scripts/color.js
@@ -5,7 +5,8 @@ const bg = '#232528'
 const fg = '#3A2B29'
 const accent = '#FFC535'
 const border = '#386150'
-const fgText = '#386150'
+const primaryText = '#386150'
+const secondaryText = '#000'
 
 module.exports = () => {
   return `
@@ -15,7 +16,8 @@ module.exports = () => {
     --fg-color: ${fg};
     --accent-color: ${accent};
     --border-color: ${border};
-    --fg-text-color: ${fgText};
+    --primary-text-color: ${primaryText};
+    --secondary-text-color: ${secondaryText};
   }
   `
 }
