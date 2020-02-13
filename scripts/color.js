a19f523d69e07e9cfe30139332c4adfc14e3ed82
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-12 23:37:34 -0800

    Group colors into an object

--- scripts/color.js
+++ scripts/color.js
@@ -1,25 +1,27 @@
 'use strict'
 
-const nav = '#3A2B29'
-const bg = '#232528'
-const fg = '#3A2B29'
-const accent = '#FFC535'
-const border = '#386150'
-const primaryText = '#386150'
-const secondaryText = '#000'
-const tertiaryText = '#fff'
+const color = {
+  nav: '#3A2B29',
+  bg: '#232528',
+  fg: '#3A2B29',
+  accent: '#FFC535',
+  border: '#386150',
+  primaryText: '#386150',
+  secondaryText: '#000',
+  tertiaryText: '#fff'
+}
 
 module.exports = () => {
   return `
   :root {
-    --nav-color: ${nav};
-    --bg-color: ${bg};
-    --fg-color: ${fg};
-    --accent-color: ${accent};
-    --border-color: ${border};
-    --primary-text-color: ${primaryText};
-    --secondary-text-color: ${secondaryText};
-    --tertiary-text-color: ${tertiaryText};
+    --nav-color: ${color.nav};
+    --bg-color: ${color.bg};
+    --fg-color: ${color.fg};
+    --accent-color: ${color.accent};
+    --border-color: ${color.border};
+    --primary-text-color: ${color.primaryText};
+    --secondary-text-color: ${color.secondaryText};
+    --tertiary-text-color: ${color.tertiaryText};
   }
   `
 }
