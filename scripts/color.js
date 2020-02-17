2c08cdc9c7fb1d83a0acc9a1817d5d93a3f076c3
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-16 17:31:06 -0800

    Create light theme

--- scripts/color.js
+++ scripts/color.js
@@ -14,12 +14,36 @@ const themes = {
   Forest: {
     nav: '#3A2B29',
     bg: '#232528',
-    fg: '#3A2B29',
-    accent: '#FFC535',
+    fg: '#232528',
+    accent: '#386150',
+    button: '#3A2B29',
+    buttonText: '#386150',
+    buttonHover: '#386150',
+    buttonTextHover: '#3A2B29',
     border: '#386150',
+    borderHover: '#3A2B29',
     primaryText: '#386150',
     secondaryText: '#000',
-    tertiaryText: '#fff'
+    tertiaryText: '#fff',
+    inputBg: '#232528',
+    inputBgFocus: '#37383B'
+  },
+  Light: {
+    nav: '#033860',
+    bg: '#fff',
+    fg: '#fff',
+    accent: 'cadetblue',
+    button: 'cadetblue',
+    buttonText: '#fff',
+    buttonHover: '#F05D5E',
+    buttonTextHover: '#fff',
+    border: 'cadetblue',
+    borderHover: 'lightblue',
+    primaryText: '#000',
+    secondaryText: '#fff',
+    tertiaryText: '#fff',
+    inputBg: '#fff',
+    inputBgFocus: '#eeeef5'
   }
 }
 
@@ -37,9 +61,16 @@ const colorStyle = theme => {
     --fg-color: ${color.fg};
     --accent-color: ${color.accent};
     --border-color: ${color.border};
+    --border-hover-color: ${color.borderHover};
+    --button-color: ${color.button};
+    --button-text-color: ${color.buttonText};
+    --button-text-hover-color: ${color.buttonTextHover};
+    --button-hover-color: ${color.buttonHover};
     --primary-text-color: ${color.primaryText};
     --secondary-text-color: ${color.secondaryText};
     --tertiary-text-color: ${color.tertiaryText};
+    --input-bg-color: ${color.inputBg};
+    --input-bg-focus-color: ${color.inputBgFocus}
   }
   `
 }
