f8e9d186ce7bb0cadc7a981daf4aef2adab2b97c
Edward Hall <edwardhall@csus.edu>
2020-02-18 12:44:24 -0800

    Add middleware/emailer.js to auth.js by Edward Hall

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const nodemailer = require('nodemailer')
+var jwt = require('jsonwebtoken')
+
+//User Authentication middleware
 const loggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next()
@@ -22,4 +26,38 @@ function isAdmin(req, res, next) {
   }
 }
 
-module.exports = { loggedIn, redirectIfLoggedIn, isAdmin }
+//Email Middleware
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS
+  }
+})
+const sendEmail = async (req, res) => {
+  try {
+    const token = jwt.sign(
+      {
+        email: req.body.email
+      },
+
+      process.env.JWT_KEY,
+      {
+        expiresIn: '12h'
+      }
+    )
+
+    const url = `http://localhost:8000/register/${token}`
+
+    await transporter.sendMail({
+      to: req.body.email,
+      subject: 'Welcome to The Farrier Center',
+      html: `Create Your Account: <a href="${url}">${url}</a>`
+    })
+  } catch (e) {
+    console.log(e)
+  }
+  res.redirect('/admin')
+}
+
+module.exports = { loggedIn, redirectIfLoggedIn, isAdmin, sendEmail }
