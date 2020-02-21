313943695d4a177ec2223fadc65ace30b5671ffd
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-20 23:59:34 -0800

    Move Light theme above Forest theme

--- scripts/color.js
+++ scripts/color.js
@@ -18,25 +18,6 @@ const themes = {
     secondaryText: '#000',
     tertiaryText: '#fff'
   },
-  Forest: {
-    nav: '#3A2B29',
-    bg: '#232528',
-    fg: '#232528',
-    accent: '#386150',
-    button: '#386150',
-    buttonText: '#3A2B29',
-    buttonHover: '#3A2B29',
-    buttonTextHover: '#386150',
-    buttonBorderHover: '#386150',
-    border: '#386150',
-    borderHover: '#3A2B29',
-    queue: '#3A2B29',
-    primaryText: '#386150',
-    secondaryText: '#000',
-    tertiaryText: '#fff',
-    inputBg: '#232528',
-    inputBgFocus: '#37383B'
-  },
   Light: {
     nav: '#033860',
     bg: '#fff',
@@ -55,6 +36,25 @@ const themes = {
     tertiaryText: '#fff',
     inputBg: '#fff',
     inputBgFocus: '#eeeef5'
+  },
+  Forest: {
+    nav: '#3A2B29',
+    bg: '#232528',
+    fg: '#232528',
+    accent: '#386150',
+    button: '#386150',
+    buttonText: '#3A2B29',
+    buttonHover: '#3A2B29',
+    buttonTextHover: '#386150',
+    buttonBorderHover: '#386150',
+    border: '#386150',
+    borderHover: '#3A2B29',
+    queue: '#3A2B29',
+    primaryText: '#386150',
+    secondaryText: '#000',
+    tertiaryText: '#fff',
+    inputBg: '#232528',
+    inputBgFocus: '#37383B'
   }
 }
 
