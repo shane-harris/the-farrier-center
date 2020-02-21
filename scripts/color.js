15cb5129cd488f24c43ec3ff89bc917e12f13066
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-21 09:33:28 -0800

    Alphabetize color definitions

--- scripts/color.js
+++ scripts/color.js
@@ -2,56 +2,56 @@
 
 const themes = {
   Dark: {
-    nav: '#222',
-    bg: '#333',
-    fg: '#222',
     accent: '#ffc535',
-    button: '#ffc535',
-    buttonText: '#222',
-    buttonHover: '#222',
-    buttonTextHover: '#ffc535',
-    buttonBorderHover: '#ffc535',
+    bg: '#333',
     border: '#000',
     borderHover: '#ffc535',
+    button: '#ffc535',
+    buttonBorderHover: '#ffc535',
+    buttonHover: '#222',
+    buttonText: '#222',
+    buttonTextHover: '#ffc535',
+    fg: '#222',
+    nav: '#222',
+    primaryText: '#aaa',
     queue: '#222',
-    queueText: '#aaa',
-    primaryText: '#aaa'
+    queueText: '#aaa'
   },
   Light: {
-    nav: '#033860',
-    bg: '#fff',
-    fg: '#fff',
     accent: 'cadetblue',
-    button: 'cadetblue',
-    buttonText: 'white',
-    buttonHover: 'white',
-    buttonTextHover: 'cadetblue',
-    buttonBorderHover: 'cadetblue',
+    bg: '#fff',
     border: 'cadetblue',
     borderHover: 'lightblue',
-    queue: '#033860',
-    queueText: 'white',
-    primaryText: '#000',
+    button: 'cadetblue',
+    buttonBorderHover: 'cadetblue',
+    buttonHover: 'white',
+    buttonText: 'white',
+    buttonTextHover: 'cadetblue',
+    fg: '#fff',
     inputBg: '#fff',
-    inputBgFocus: '#eeeef5'
+    inputBgFocus: '#eeeef5',
+    nav: '#033860',
+    primaryText: '#000',
+    queue: '#033860',
+    queueText: 'white'
   },
   Forest: {
-    nav: '#3A2B29',
-    bg: '#232528',
-    fg: '#232528',
     accent: '#386150',
-    button: '#386150',
-    buttonText: '#3A2B29',
-    buttonHover: '#3A2B29',
-    buttonTextHover: '#386150',
-    buttonBorderHover: '#386150',
+    bg: '#232528',
     border: '#386150',
     borderHover: '#3A2B29',
-    queue: '#3A2B29',
-    queueText: '#aaa',
-    primaryText: '#386150',
+    button: '#386150',
+    buttonBorderHover: '#386150',
+    buttonHover: '#3A2B29',
+    buttonText: '#3A2B29',
+    buttonTextHover: '#386150',
+    fg: '#232528',
     inputBg: '#232528',
-    inputBgFocus: '#37383B'
+    inputBgFocus: '#37383B',
+    nav: '#3A2B29',
+    primaryText: '#386150',
+    queue: '#3A2B29',
+    queueText: '#aaa'
   }
 }
 
@@ -64,22 +64,22 @@ const colorStyle = theme => {
   const color = theme !== undefined ? themes[theme] : themes.Dark
   return `
   :root {
-    --nav-color: ${color.nav};
-    --bg-color: ${color.bg};
-    --fg-color: ${color.fg};
     --accent-color: ${color.accent};
+    --bg-color: ${color.bg};
     --border-color: ${color.border};
     --border-hover-color: ${color.borderHover};
     --button-color: ${color.button};
-    --button-text-color: ${color.buttonText};
     --button-border-color: ${color.buttonBorderHover};
-    --button-text-hover-color: ${color.buttonTextHover};
     --button-hover-color: ${color.buttonHover};
+    --button-text-color: ${color.buttonText};
+    --button-text-hover-color: ${color.buttonTextHover};
+    --fg-color: ${color.fg};
+    --input-bg-color: ${color.inputBg};
+    --input-bg-focus-color: ${color.inputBgFocus};
+    --nav-color: ${color.nav};
     --primary-text-color: ${color.primaryText};
     --queue-color: ${color.queue};
     --queue-text-color: ${color.queueText};
-    --input-bg-color: ${color.inputBg};
-    --input-bg-focus-color: ${color.inputBgFocus}
   }
   `
 }
