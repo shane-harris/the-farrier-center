04d540771a3d781f529f227c8597367e6dfd70b9
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-23 20:58:25 -0800

    Add tests for 'util.js'
    Use 'valueOr' in 'style.js'

--- middleware/style.js
+++ middleware/style.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const { colorStyle, themes } = require('../scripts/color')
+const { valueOr } = require('../scripts/util')
 
 // Middleware that will import 'style' into every EJS file
 module.exports = (req, res, next) => {
-  const theme = req.user !== undefined ? req.user.theme : 'Dark'
+  const theme = valueOr(req.user, 'Dark')
   res.locals.style = colorStyle(theme)
   res.locals.themes = themes(theme)
   next()
