0fc06aff682972dc1c837250fed1fc0ff3db547c
Edward Hall <edwardhall@csus.edu>
2020-02-23 16:24:12 -0800

    create image schema by Edward Hall

--- models/image.js
+++ models/image.js
@@ -2,12 +2,26 @@
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const Horses = require('./horse')
+const Medicals = require('./medical')
+const Shoeings = require('./shoeing')
+const Users = require('./user')
 
 const ImageSchema = new Schema({
-  id: Number,
+  ref_id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    refPath: 'onType'
+  },
+  onType: {
+    type: String,
+    required: true,
+    enum: ['horses', 'medical', 'shoeing', 'User']
+  },
+  public_id: String,
   url: String
 })
 
-const Image = mongoose.Schema('image', ImageSchema)
+const Image = mongoose.model('images', ImageSchema)
 
 module.exports = Image
