61e0d4b9cd8bbc9b15f5740453eb55f47130f713
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-24 23:00:19 -0800

    Fix bug causing theme not to load properly

--- middleware/style.js
+++ middleware/style.js
@@ -5,7 +5,8 @@ const { valueOr } = require('../scripts/util')
 
 // Middleware that will import 'style' into every EJS file
 module.exports = (req, res, next) => {
-  const theme = valueOr(req.user, 'Dark')
+  const user = valueOr(req.user, { theme: 'Dark' })
+  const theme = valueOr(user.theme, 'Dark')
   res.locals.style = generateStyle(theme)
   res.locals.themes = themes(theme)
   next()
