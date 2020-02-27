227da7f249d303f94a6ca320e3abb954c0ad8a77
Edward Hall <edwardhall@csus.edu>
2020-02-27 15:40:59 -0800

    update tests to cover newly created routes by Edward Hall

--- test/routes.js
+++ test/routes.js
@@ -6,38 +6,7 @@ chai.should()
 const app = require('../index')
 
 describe('Routes', () => {
-  describe('GET /horse/all', () => {
-    let req
-    before(() => {
-      return (req = chai.request(app).get('/horse/all'))
-    })
-
-    it('should get the horses page', () => {
-      return req.then(res => res.should.have.status(200))
-    })
-    describe('The horses page', () => {
-      it('should render HTML', () => {
-        return req.then(res => res.should.be.html)
-      })
-    })
-  })
-
-  describe('GET /horse/1', () => {
-    let req
-    before(() => {
-      return (req = chai.request(app).get('/horse/1'))
-    })
-
-    it('should get the horse page', () => {
-      return req.then(res => res.should.have.status(200))
-    })
-    describe('The horse page', () => {
-      it('should render HTML', () => {
-        return req.then(res => res.should.be.html)
-      })
-    })
-  })
-
+  //Tests below
   describe('GET /', () => {
     let req
     before(() => {
@@ -72,6 +41,23 @@ describe('Routes', () => {
     })
   })
 
+  describe('GET /user/theme', () => {
+    let req
+    before(() => {
+      return (req = chai.request(app).get('/user/theme'))
+    })
+
+    it('should get the user page', () => {
+      return req.then(res => res.should.have.status(200))
+    })
+
+    describe('The user theme page', () => {
+      it('should render HTML', () => {
+        return req.then(res => res.should.be.html)
+      })
+    })
+  })
+
   describe('GET /horse/queue', () => {
     let req
     before(() => {
@@ -83,6 +69,106 @@ describe('Routes', () => {
     })
 
     describe('The queue page', () => {
+      it('should render HTML', () => {
+        return req.then(res => res.should.be.html)
+      })
+    })
+  })
+
+  describe('GET /horse/all', () => {
+    let req
+    before(() => {
+      return (req = chai.request(app).get('/horse/all'))
+    })
+
+    it('should get the all horses page', () => {
+      return req.then(res => res.should.have.status(200))
+    })
+    describe('The all horses page', () => {
+      it('should render HTML', () => {
+        return req.then(res => res.should.be.html)
+      })
+    })
+  })
+
+  describe('GET /horse/new', () => {
+    let req
+    before(() => {
+      return (req = chai.request(app).get('/horse/new'))
+    })
+
+    it('should get the new horse page', () => {
+      return req.then(res => res.should.have.status(200))
+    })
+
+    describe('The new horse page', () => {
+      it('should render HTML', () => {
+        return req.then(res => res.should.be.html)
+      })
+    })
+  })
+
+  describe('GET /horse/1/new-medical-analysis', () => {
+    let req
+    before(() => {
+      return (req = chai.request(app).get('/horse/1/new-medical-analysis'))
+    })
+
+    it('should get the new medical analysis page', () => {
+      return req.then(res => res.should.have.status(200))
+    })
+
+    describe('The new medical analysis page', () => {
+      it('should render HTML', () => {
+        return req.then(res => res.should.be.html)
+      })
+    })
+  })
+
+  describe('GET /horse/1/new-shoeing', () => {
+    let req
+    before(() => {
+      return (req = chai.request(app).get('/horse/1/new-shoeing'))
+    })
+
+    it('should get the new shoeing page', () => {
+      return req.then(res => res.should.have.status(200))
+    })
+
+    describe('The new shoeing page', () => {
+      it('should render HTML', () => {
+        return req.then(res => res.should.be.html)
+      })
+    })
+  })
+
+  describe('GET /horse/1/update', () => {
+    let req
+    before(() => {
+      return (req = chai.request(app).get('/horse/1/update'))
+    })
+
+    it('should get the update horse page', () => {
+      return req.then(res => res.should.have.status(200))
+    })
+
+    describe('The update horse page', () => {
+      it('should render HTML', () => {
+        return req.then(res => res.should.be.html)
+      })
+    })
+  })
+
+  describe('GET /horse/1', () => {
+    let req
+    before(() => {
+      return (req = chai.request(app).get('/horse/1'))
+    })
+
+    it('should get the horse page', () => {
+      return req.then(res => res.should.have.status(200))
+    })
+    describe('The horse page', () => {
       it('should render HTML', () => {
         return req.then(res => res.should.be.html)
       })
