a3d072ce0c5a8e4f31a9330da4629337dae52cb4
Michael Dorst <dorstm@ecs.csus.edu>
2020-02-29 22:12:21 -0800

    Add navbar tests

--- test/front-end/queue.js
+++ test/front-end/queue.js
@@ -2,8 +2,11 @@
 
 const chai = require('chai')
 chai.should()
+const { By } = require('selenium-webdriver')
 const { getDriver, logIn } = require('./driver')
 const driver = getDriver()
+
+const route = route => `http://localhost:9090${route}`
 
 describe('Queue', () => {
   before(async function() {
@@ -14,15 +17,39 @@ describe('Queue', () => {
     return logIn('test', 'test')
   })
 
+  beforeEach(async () => await driver.get(route('/horse/queue')))
+
   it('Has the correct title', async () => {
     const title = await driver.getTitle()
-    await title.should.equal('Horse Queue | Farrier Center')
+    title.should.equal('Horse Queue | Farrier Center')
   }).timeout(10000)
 
-  after(async () => {
-    if (process.env.TEST_FRONT_END === 'false') {
-      return
+  describe('The navbar', () => {
+    it('Should exist', async () => {
+      const found = await driver.findElements(By.id('header-nav'))
+      found.length.should.equal(1)
+    })
+
+    const shouldHaveLink = (text, route) => {
+      it(`Should have ${text} link`, async () => {
+        const url = await driver
+          .findElement(By.id('header-nav'))
+          .findElement(By.linkText(text))
+          .getAttribute('href')
+        url.should.equal(route)
+      })
     }
-    return driver.get('http://localhost:9090/logout')
+
+    shouldHaveLink('The Farrier Center', route('/'))
+    shouldHaveLink('Queue', route('/horse/queue'))
+    shouldHaveLink('Horses', route('/horse/all'))
+    shouldHaveLink('New Horse', route('/horse/new'))
+    shouldHaveLink('Admin', route('/admin'))
+  })
+
+  after(async () => {
+    if (process.env.TEST_FRONT_END !== 'false') {
+      return driver.get(route('/logout'))
+    }
   })
 })
