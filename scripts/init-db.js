f4941097f3e3a497d61e57b6c41994a366de20a3
Michael Dorst <dorstm@ecs.csus.edu>
2020-03-22 13:58:11 -0700

    Use async/await in 'index.js' and 'scripts/init-db.js'

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -2,7 +2,7 @@
 
 const User = require('../models/user')
 
-const makeDefaultAdmin = () => {
+const makeDefaultAdmin = async () => {
   const defaultUser = {
     fname: 'admin',
     lname: 'admin',
@@ -12,20 +12,19 @@ const makeDefaultAdmin = () => {
     password: 'farrierdev'
   }
 
-  User.findOne({ email: 'farrierdev@gmail.com' }, (err, user) => {
-    if (user) {
-      console.log('Found default admin, skipping creation...')
-    } else {
-      console.log('Default admin not found, creating user...')
-      User.register(new User(defaultUser), defaultUser.password, err => {
-        if (err) {
-          console.log('error while user register!', err)
-        } else {
-          console.log('default admin successfuly created')
-        }
-      })
-    }
-  })
+  const user = await User.findOne({ email: 'farrierdev@gmail.com' })
+  if (user) {
+    console.log('Found default admin, skipping creation...')
+  } else {
+    console.log('Default admin not found, creating user...')
+    User.register(new User(defaultUser), defaultUser.password, err => {
+      if (err) {
+        console.log('error while user register!', err)
+      } else {
+        console.log('default admin successfuly created')
+      }
+    })
+  }
 }
 
 module.exports = { makeDefaultAdmin }
