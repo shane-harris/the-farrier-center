49b6bc45cb32892949dfa4cadf77f457cb2874c1
Shane Harris <shaneharris@csus.edu>
2020-04-01 21:26:58 -0700

    All kind of changes to search WIP. Shane Harris

--- routes/index.js
+++ routes/index.js
@@ -63,16 +63,52 @@ router.get('/logout', (req, res) => {
 })
 
 router.get('/search', loggedIn, async (req, res) => {
-  const horses = await Horse.find()
+  console.log("Res from search...............................................", req.url.normalize())
+  let pureQuery = req.url.replace("/search?query=", "");
+  pureQuery = pureQuery.replace("%20", " ") //Remove the special character correction later
+  pureQuery = pureQuery.replace("%2C%20", ", ")
+  pureQuery = pureQuery.replace("%20", " ")
+  let test = req.param.query
+  console.log("Hope this works", test)
+  console.log("Pure query...", pureQuery)
+
+  let horses = await Horse.find({ name: pureQuery })
     // sort by id (ascending)
     .sort({ id: 1 })
+
+  if (horses.length === 0) {
+    horses = await Horse.find({ owner: pureQuery })
+      .sort({ id: 1 })
+  }
   res.render('search.ejs', { username: req.user.username, horses: horses })
+
 })
 
 router.post('/search', async (req, res) => {
   try {
-    const found = await Horse.findOne({ name: req.body.query })
-    res.redirect(`/horse/${found.id}`)
+    const howMany = await Horse.find({ name: req.body.query })
+    const howManyOwners = await Horse.find({ owner: req.body.query })
+    console.log("How many have same name", howMany.length)
+
+    console.log("How many...", howMany)
+    console.log(howMany[0].id)
+    if (howMany.length === 1) {
+      res.redirect(`/horse/${howMany[0].id}`)
+    }
+    else {
+      res.redirect(
+        url.format({
+          pathname: '/search',
+          query: {
+            query: req.body.query
+          }
+        })
+      )
+      //console.log(req.body.query, `Horse '${req.body.query}' not found.`)
+    }
+
+    //const found = await Horse.findOne({ name: req.body.query })
+    //res.redirect(`/horse/${found.id}`)
   } catch (_) {
     res.redirect(
       url.format({
@@ -82,22 +118,27 @@ router.post('/search', async (req, res) => {
         }
       })
     )
-    console.log(req.body.query, `Horse '${req.body.query}' not found.`)
+    //console.log(req.body.query, `Horse '${req.body.query}' not found.`)
   }
 })
 
 router.get('/autocomplete', loggedIn, (req, res) => {
   const regex = new RegExp(req.query["term"], 'i');
   //var horseOwner = new RegExp(req.query["term"], 'i');
-
+  var result = [];
   var horseFilter = Horse.find({ name: regex }, { 'name': 1 })
     .sort({ "updated_at": -1 })
     .sort({ "created_at": -1 })
-    .limit(20);
+    .limit(100)
+
+  var horseOwner = Horse.find({ name: regex }, { 'owner': 1 })
+    .sort({ "updated_at": -1 })
+    .sort({ "created_at": -1 })
+    .limit(100)
 
   horseFilter.exec(function (err, data) {
     //data is all horse names that match query
-    var result = [];
+    //var result = [];
     if (!err) {
       if (data && data.length && data.length > 0) {
         data.forEach(horse => {
@@ -110,9 +151,29 @@ router.get('/autocomplete', loggedIn, (req, res) => {
         });
       }
       //return all the results to autocomplete.js
-      res.jsonp(result);
+      //res.jsonp(result);
     }
   });
+
+  horseOwner.exec(function (err, data) {
+    //data is all horse names that match query
+    //var result = [];
+    if (!err) {
+      if (data && data.length && data.length > 0) {
+        data.forEach(horse => {
+          console.log("data within horse", horse);
+          let obj = { //turn each horse name and id pair into an opject
+            id: horse._id, //dont know why horse.id returns undefined but _id works
+            label: horse.owner,
+          };
+          result.push(obj);
+        });
+      }
+      //return all the results to autocomplete.js
+      // res.jsonp(result);
+    }
+  });
+  res.jsonp(result);
 })
 
 module.exports = router
