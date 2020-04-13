ee4b90d87e0e9fba307ae00be326d6cf9027599f
Michael Dorst <dorstm@ecs.csus.edu>
2020-04-13 11:29:20 -0700

    Remove "I'm feeling lucky"
    Search in the middle of words, not just the beginning

--- routes/index.js
+++ routes/index.js
@@ -64,25 +64,19 @@ router.get('/logout', (req, res) => {
 })
 
 router.get('/search', loggedIn, async (req, res) => {
-  const pureQuery = decodeURI(req.url.replace('/search?query=', ''))
+  const query = decodeURI(req.url.replace('/search?query=', ''))
 
-  let horses = await Horse.find({ name: pureQuery }).sort({ id: 1 })
+  const pattern = `.*${query}.*`
 
-  if (horses.length === 0) {
-    horses = await Horse.find({ owner: pureQuery }).sort({ id: 1 })
+  const [horsesByName, horsesByLocation, horsesByOwner] = await Promise.all([
+    Horse.find({ name: { $regex: pattern, $options: 'i' } }).sort({ name: 1 }),
+    Horse.find({ location: { $regex: pattern, $options: 'i' } }).sort({ location: 1 }),
+    Horse.find({ owner: { $regex: pattern, $options: 'i' } }).sort({ owner: 1 })
+  ])
 
-    if (horses.length === 0) {
-      horses = await Horse.find({ location: pureQuery }).sort({ id: 1 })
+  const horses = horsesByName.concat(horsesByLocation).concat(horsesByOwner)
 
-      /*No horses found via current query.
-      Search for all matches that begin with query*/
-      if (horses.length === 0) {
-        const pattern = pureQuery.replace(pureQuery, "^" + pureQuery + ".*")
-        horses = await Horse.find({ name: { $regex: pattern, $options: "i" } })
-      }
-    }
-  }
-  res.render('search.ejs', { username: req.user.username, horses: horses })
+  res.render('search.ejs', { username: req.user.username, horses })
 })
 
 router.post('/search', async (req, res) => {
@@ -106,14 +100,11 @@ router.get('/autocomplete', loggedIn, async (req, res) => {
   const regex = new RegExp(req.query['term'], 'i')
 
   const [horseName, horseOwner, horseLocation] = await Promise.all([
-    Horse.find({ name: regex }, { name: 1 })
-      .limit(30),
+    Horse.find({ name: regex }, { name: 1 }).limit(30),
 
-    Horse.find({ owner: regex }, { owner: 1 })
-      .limit(30),
+    Horse.find({ owner: regex }, { owner: 1 }).limit(30),
 
-    Horse.find({ location: regex }, { location: 1 })
-      .limit(30)
+    Horse.find({ location: regex }, { location: 1 }).limit(30)
   ])
 
   const horses = horseOwner.concat(horseLocation).concat(horseName)
@@ -126,7 +117,8 @@ router.get('/autocomplete', loggedIn, async (req, res) => {
   //Removing duplicate results from full result array
   const result = fullResult.filter(
     (item, index) =>
-      fullResult.indexOf(fullResult.find(found => found.label === item.label)) === index)
+      fullResult.indexOf(fullResult.find(found => found.label === item.label)) === index
+  )
   res.jsonp(result)
 })
 
