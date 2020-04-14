b1a75f7e1cda60d7591320f5e164d338aaa1a03b
Edward Hall <edwardhall@csus.edu>
2020-04-14 12:29:20 -0700

    Update routes and views to use Report schema

--- routes/horse.js
+++ routes/horse.js
@@ -4,10 +4,8 @@ const express = require('express')
 const router = express.Router()
 
 //Import mongoose models
-const Medical = require('../models/medical')
 const Horse = require('../models/horse')
-const Horseshoe = require('../models/horseshoe')
-const Shoeing = require('../models/shoeing')
+const Report = require('../models/report')
 const Image = require('../models/image')
 
 //import middlewares
@@ -32,6 +30,16 @@ const storage = cloudinaryStorage({
 })
 
 const parser = multer({ storage })
+
+//Fields used to parse images from /:id/new-reports route
+const imageFields = [
+  { name: 'backLeftImages', maxCount: 6 },
+  { name: 'backRightImages', maxCount: 6 },
+  { name: 'frontLeftImages', maxCount: 6 },
+  { name: 'frontRightImages', maxCount: 6 },
+  { name: 'notesImages', maxCount: 6 },
+  { name: 'medicalImages', maxCont: 6 }
+]
 
 router.use('/public', express.static('public'))
 
@@ -81,97 +89,175 @@ router.post('/new', loggedIn, parser.single('image'), async (req, res) => {
   await horse.save()
 
   if (req.body.submit === 'shoeing') {
-    res.redirect(`/horse/${horse.id}/new-shoeing`)
+    res.redirect(`/horse/${horse.id}/new-report`)
   } else {
     res.redirect('/horse/all')
   }
 })
 
 router.get('/:id', loggedIn, async (req, res) => {
-  const [horse, medicals, shoeings] = await Promise.all([
+  const [horse, shoeing] = await Promise.all([
     Horse.findOne({ id: req.params.id }).populate('image'),
-    Medical.find({ horse_id: req.params.id }).sort({ date: -1 }),
-    Shoeing.find({ horse_id: req.params.id }).sort({ date: -1 })
+    Report.find({ horse_id: req.params.id }).sort({ date: -1 })
   ])
+  console.log(shoeing[0])
   res.render('horse.ejs', {
     horse,
-    medical: medicals[0],
-    shoeing: shoeings[0],
-    updateable: medicals.length !== 0
+    shoeing: shoeing[0],
+    updateable: false
   })
 })
 
-router.get('/:id/new-medical-analysis', loggedIn, async (req, res) => {
-  const [horse, medical] = await Promise.all([
+router.get('/:id/new-report', loggedIn, async (req, res) => {
+  const [horse, shoeing] = await Promise.all([
     Horse.findOne({ id: req.params.id }),
     // Get the most recent medical analysis
-    Medical.findOne({ horse_id: req.params.id }).sort({ date: -1 })
+    Report.find({ horse_id: req.params.id }).sort({ date: -1 })
   ])
-  res.render('new-medical-analysis.ejs', { horse, medical, updateable: !!medical })
-})
-
-router.post('/:id/new-medical-analysis', loggedIn, async (req, res) => {
-  const medical = new Medical({
-    horse_id: req.params.id,
-    date: new Date(),
-    farrier: 'Default Steve',
-    ...req.body
+  console.log(shoeing)
+  res.render('new-report.ejs', {
+    horse,
+    medical: shoeing.medical,
+    updateable: shoeing.medical === undefined ? false : true,
+    scripts: require('../scripts/shoeing-events')
   })
-  medical.save()
-  // When you make a new medical, update horse's last visit date.
-  const horse = await Horse.findOne({ id: req.params.id })
-  horse.lastVisit = medical.date
-  horse.save()
-  res.redirect(`/horse/${req.params.id}`)
 })
 
-router.get('/:id/new-shoeing', loggedIn, async (req, res) => {
+router.post('/:id/new-report', parser.fields(imageFields), loggedIn, async (req, res) => {
   const horse = await Horse.findOne({ id: req.params.id })
-  res.render('new-shoeing.ejs', { horse })
-})
-
-router.post('/:id/new-shoeing', loggedIn, async (req, res) => {
-  console.log(req.body)
-  const shoeing = new Shoeing({
+  const report = new Report({
     horse_id: req.params.id,
     date: new Date(), //returns todays date
-    farrier: 'Default Steve',
-    frontLeft: new Horseshoe({
-      jobType: req.body.frontLeftShoe, //full, half, trim
-      shoeSize: req.body.frontLeftSize,
-      notes: req.body.frontLeftNotes,
-      hoofImage1: null,
-      hoofImage2: null
-    }),
-    frontRight: new Horseshoe({
-      jobType: req.body.frontRightShoe,
-      shoeSize: req.body.frontRightSize,
-      notes: req.body.frontRightNotes,
-      hoofImage1: null,
-      hoofImage2: null
-    }),
-    backLeft: new Horseshoe({
-      jobType: req.body.backLeftShoe,
-      shoeSize: req.body.backLeftSize,
-      notes: req.body.backLeftNotes,
-      hoofImage1: null,
-      hoofImage2: null
-    }),
-    backRight: new Horseshoe({
-      jobType: req.body.backRightShoe,
-      shoeSize: req.body.backRightSize,
-      notes: req.body.backRightNotes,
-      hoofImage1: null,
-      hoofImage2: null
-    }),
-
-    ...req.body
+    farrier: req.user.username,
+    jobType: req.body.job,
+    front: {}
   })
-  shoeing.save()
-
   //When you make a new shoeing, update horses last visit date.
-  const horse = await Horse.findOne({ id: req.params.id })
-  horse.lastVisit = shoeing.date
+  horse.lastVisit = report.date
+
+  //Medical Analysis Info
+  if (
+    req.body.gait ||
+    req.body.lameness ||
+    req.body.blemishes ||
+    req.body.laminitus ||
+    req.body.notes
+  ) {
+    report.medical = {
+      gait: req.body.gait,
+      lameness: req.body.lameness,
+      blemishes: req.body.blemishes,
+      laminitus: req.body.laminitus,
+      notes: req.body.notes
+    }
+  }
+
+  //If there are any shoeing info for front area left = horseshoe[0], right = horseshoe[1]
+  if (req.body.job === 'Half' || req.body.job === 'Full' || req.body.job === 'Trim') {
+    if (req.body.shoes) {
+      for (const shoe of req.body.shoes) {
+        report.front.shoes.push(shoe)
+      }
+    }
+    if (req.body.materials) {
+      for (const material of req.body.materials) {
+        report.front.materials.push(material)
+      }
+    }
+    if (req.body.services) {
+      for (const service of req.body.services) {
+        report.front.services.push(service)
+      }
+    }
+
+    report.front.horseshoes.push({
+      hoof: 'Left',
+      shoeSize: req.body.frontLeftSize,
+      notes: req.body.frontLeftNotes
+    })
+    report.front.horseshoes.push({
+      hoof: 'Right',
+      shoeSize: req.body.frontRightSize,
+      notes: req.body.frontRightNotes
+    })
+  }
+  //This catches the back area if there is any shoeing info for both front and back areas
+  //left = horseshoe[0], right = horseshoe[1]
+  if (req.body.job === 'Full' || req.body.job === 'Trim') {
+    report.back.horseshoes.push({
+      hoof: 'Left',
+      shoeSize: req.body.backLeftSize,
+      notes: req.body.backLeftNotes
+    })
+    report.back.horseshoes.push({
+      hoof: 'Right',
+      shoeSize: req.body.backRightSize,
+      notes: req.body.backRightNotes
+    })
+  }
+
+  //If there are any images
+  if (req.files) {
+    for (const field in req.files) {
+      //each array in parser object, <= 6 fields
+      const fieldname = field.substring(0, field.length - 6) //minus "Image" as named in horseshoe schema
+
+      for (const image of field) {
+        //each file from the field array, <= 6 files
+        const pic = new Image({
+          ref_id: report._id,
+          onType: 'report',
+          url: image.url,
+          public_id: image.public_id
+        }).save()
+
+        //save image _id to relevant area
+        switch (fieldname) {
+          case 'frontLeft':
+            if (report.front.horseshoes[0].hoof === 'Left') {
+              report.front.horseshoes[0].images.push(pic._id)
+            } else if (report.front.horseshoes[1].hoof === 'Left') {
+              report.front.horseshoes[1].images.push(pic._id)
+            }
+            break
+
+          case 'frontRight':
+            if (report.front.horseshoes[1].hoof === 'Right') {
+              report.front.horseshoes[1].images.push(pic._id)
+            } else if (report.front.horseshoes[0].hoof === 'Right') {
+              report.front.horseshoes[0].images.push(pic._id)
+            }
+            break
+
+          case 'backLeft':
+            if (report.back.horseshoes[0].hoof === 'Left') {
+              report.back.horseshoes[0].images.push(pic._id)
+            } else if (report.back.horseshoes[1].hoof === 'Left') {
+              report.back.horseshoes[1].images.push(pic._id)
+            }
+            break
+
+          case 'backRight':
+            if (report.back.horseshoes[1].hoof === 'Right') {
+              report.back.horseshoes[1].images.push(pic._id)
+            } else if (report.back.horseshoes[0].hoof === 'Right') {
+              report.back.horseshoes[0].images.push(pic._id)
+            }
+            break
+
+          case 'medical':
+            report.medical.images.push(pic._id)
+            break
+
+          case 'notes':
+            report.images.push(pic._id)
+            break
+        }
+      }
+    }
+  }
+  //If there are images to save the references are saved when shoeing is saved. Only saves subdocuments when shoeing is saved
+  report.save()
   horse.save()
   res.redirect(`/horse/${req.params.id}`)
 })
