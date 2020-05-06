8a7723c2bdd7eb34368bdbb37ec96e7ad7bc30d5
Sam Hendryx <hendryx@ecs.csus.edu>
2020-05-05 17:11:04 -0700

    Fix bug in queue preventing non-admins from seeing view horse checkbox and sorting cols in queue.js by Sam

--- public/javascripts/queue.js
+++ public/javascripts/queue.js
@@ -4,68 +4,6 @@
 /* global Hammer, horses, user, users */
 
 /**
- * Shorten the user's name based on the width of the container it will be displayed in.
- *
- * Displays as many characters of the first name as it can, and either the last initial, or the full
- * last name if there is enough space for the entire name (first and last).
- *
- * Note: this makes a _guess_ at how many characters can probably be displayed. It may not be
- * perfect. We will try to err on the side of too short rather than too long, to avoid overlapping
- * text.
- * @param {String} fname First name
- * @param {String} lname Last name
- * @param {Number} width The width of the window
- * @returns {String} The (possibly) shortened name
- */
-function shortenName(fname, lname, width) {
-  // We don't know the exact width of the element, so we approximate how many characters we can fit
-  // loosely based on the width of the window. Change this conversion factor if it's too short or
-  // too long.
-  const conversionFactor = 1 / 77
-  // Set a minumum number of characters to 5 (Two initials, two dots and a space - i.e. "M. D.")
-  const charsAllowed = width * conversionFactor < 5 ? 5 : width * conversionFactor
-
-  const fullNameLength = fname.length + lname.length + 1
-  if (charsAllowed >= fullNameLength) {
-    return `${fname} ${lname}`
-  }
-  // It takes 3 chars to show a last initial (space, initial, and '.')
-  if (charsAllowed >= fname.length + 3) {
-    return `${fname} ${lname.charAt(0)}.`
-  }
-  // Truncate first name to length charsAllowed - 4 (leaving 4 chars for last initial part)
-  return `${fname.slice(0, charsAllowed - 4)}. ${lname.charAt(0)}.`
-}
-
-/**
- * Fill in the names of the assigned farriers for each horse
- */
-function showAssignedFarriers() {
-  // Get the width (in pixels) of the browser window
-  const width = $(window).width()
-
-  $('.assigned-farrier').each(function() {
-    // The HTML id is set to the horse's id, so we can get it with:
-    const id = $(this).attr('id')
-    // Find a horse with a matching id (there should only be one)
-    const horse = horses.find(horse => horse.id === Number(id))
-
-    if (horse.assignedFarrier === undefined) {
-      $(this).text('None')
-    } else {
-      // Get the assigned farrier
-      const farrier = users.find(user => user.id === horse.assignedFarrier)
-      const { fname, lname } = farrier
-      $(this).text(shortenName(fname, lname, width))
-    }
-  })
-}
-
-showAssignedFarriers()
-
-$(window).resize(showAssignedFarriers)
-
-/**
  * Filters the queue to show only the horses assigned to the current user.
  * Uses a custom DataTable search to filter down the list.
  */
@@ -144,9 +82,14 @@ $(document).ready(function() {
   hammertime.on('swiperight', dismiss)
 })
 
+/**
+ * Handles the submitting of the drop down boxes and sends the
+ * horse id and farrier id to the route. The Page is not reloaded
+ * and this results in a snappier interface.
+ *
+ * @param {string} horse: horse id retrieved from queue-item.ejs form
+ */
 function changeFarrier(horse) {
   const farrier = document.getElementById(horse + '-farrier').value
-  $.post('/horse/assign/' + horse + '/' + farrier, function() {
-    //location.reload()
-  })
+  $.post('/horse/assign/' + horse + '/' + farrier)
 }
